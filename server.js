08efe96b9420f20cad6fd66a9cc7707080872a6e
Atila Romero <atilaromero@gmail.com>
2017-10-02 10:41:45 -0300

    dont check old events anymore

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ const config = require('config')
 const mongodb = require('mongodb')
 const assert = require('assert')
 
-const start = new Date()
-const url = config.EVENTS_URL + '?stream=true&since=' + start.toISOString()
-console.log(url)
-const events = new EventSource(url)
+const urlStream = config.EVENTS_URL + '?stream=true'
 
 const mongoUrl = config.MONGO_URL
 const mongoClient = new mongodb.MongoClient()
@@ -16,14 +13,25 @@ const mongoClient = new mongodb.MongoClient()
 mongoClient.connect(mongoUrl, (err, db) => {
   assert.equal(null, err)
   const collection = db.collection(config.COLLECTION)
+  const events = new EventSource(urlStream)
   events.onany = (channel, message) => {
     const data = message.data && JSON.parse(message.data)
-    if (data && 'evidence' in data) {
-      const text = data.date + ' ' + data.type
-      collection.update(
-        {_id: mongodb.ObjectId(data.evidence)},
-        {$set: {progress: text}}
-      )
-    }
+    updateProgress(collection, data)
   }
 })
+
+function updateProgress (collection, data) {
+  if (data && 'evidence' in data) {
+    const text = data.date + ' ' + data.type
+    let id = data.evidence
+    try {
+      id = mongodb.ObjectId(data.evidence)
+    } catch (err) {
+    }
+    // console.log(
+    collection.update(
+      {_id: id},
+      {$set: {progress: text}}
+    )
+  }
+}
